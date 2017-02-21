7ed5b6e472e9617ebf32653564b1ea1dcf39586e
VTsantilis <vtsantilis@gmail.com>
2017-02-21 16:47:57 +0200

    add padding for body when fixed nav

--- sites/all/themes/agage/js/last.js
+++ sites/all/themes/agage/js/last.js
@@ -3,5 +3,6 @@
 		$('html').on('click', '.toggleme', function() {
 			$(this).next('.togg').toggle();
 		});
+		$('body').css('padding-top', $('.navbar-fixed-top').height() + 10);
 	});
 })(jQuery);
